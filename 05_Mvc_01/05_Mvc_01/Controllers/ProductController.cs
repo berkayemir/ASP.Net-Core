4c12bcb65c2cae082168f529de637e99eebd21df
berkayemir <brky_emr92@windowslive.com>
2022-04-06 14:56:13 +0300

    ASP.Net Core

---
+++ 05_Mvc_01/05_Mvc_01/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _05_Mvc_01.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetCategories() 
+        {
+            return View();
+        }
+    }
+}
