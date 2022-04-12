ba5c8c107bc0111c11d0e47a53b2f8741e7e3857
berkayemir <brky_emr92@windowslive.com>
2022-04-12 14:51:37 +0300

    DikenComTr_Clone

---
+++ DikenComeTr_Clone/DikenComeTr_Clone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DikenComeTr_Clone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
