909ffdbd4ea7a2887cc056aa615515c6c86ec0e8
berkayemir <brky_emr92@windowslive.com>
2022-04-15 14:55:31 +0300

    EntityFrameWork

---
+++ EntityFrameWork/EF_04_VSC/EF_04_MVC_VSC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EF_04_MVC_VSC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
