37934ba9a47040ae1b7c20e0c9e45ab46c7ac470
berkayemir <brky_emr92@windowslive.com>
2022-04-21 15:03:14 +0300

    Code-First

---
+++ EntityFrameWork/EF_CodeFirst/Migrations/20220421110849_Mig_2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EF_CodeFirst.Migrations
+{
+    public partial class Mig_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Authors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Authors");
+        }
+    }
+}
