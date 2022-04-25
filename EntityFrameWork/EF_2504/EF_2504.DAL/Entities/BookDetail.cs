4e3bf6575d01a7ddb55eb51789b46abc1ed68fb2
berkayemir <brky_emr92@windowslive.com>
2022-04-25 14:58:22 +0300

    KatmanlıMimari(25.04)

---
+++ EntityFrameWork/EF_2504/EF_2504.DAL/Entities/BookDetail.cs
@@ -0,0 +1,13 @@
+﻿namespace EF_2504.DAL.Entities
+{
+    public class BookDetail
+    {
+        public int BookDetailId { get; set; }
+        public string BookDetailIsbn { get; set; }
+        public string BookDetailCity { get; set; }
+        public string BookDetailCountry { get; set; }
+        public int BookDetailYear { get; set; }
+
+
+    }
+}
